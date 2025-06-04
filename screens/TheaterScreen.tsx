b4373d3e09cc27aefea021cdb0c59be8722fbe98
Dani Kensy <kensydanielle@gmail.com>
2025-06-04 15:48:09 -0300

    feat: theater screen

---
+++ screens/TheaterScreen.tsx
@@ -0,0 +1,167 @@
+import { Ionicons } from '@expo/vector-icons';
+import { DrawerScreenProps } from '@react-navigation/drawer';
+import { useFocusEffect } from 'expo-router';
+import React, { useCallback, useState } from 'react';
+import { ActivityIndicator, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { DrawerParamList } from '../navigation/DrawerNavigator';
+
+type Props = DrawerScreenProps<DrawerParamList, 'Theaters'>;
+
+export type Theater = {
+    id: number;
+    name: string;
+    address: string;
+    openingHours: string;
+    contactNumber: string;
+};
+
+const TheaterScreen = ({ navigation }: Props) => {
+
+    const [theaters, setTheaters] = useState<Theater[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchTheaters = async () => {
+        setLoading(true);
+        const response = await fetch('http://localhost:8000/cinemas/');
+        const data = await response.json();
+        setTheaters(data);
+        setLoading(false);
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchTheaters();
+        }, [])
+    );
+
+    const handleDelete = async (id: number) => {
+        const res = await fetch(`http://localhost:8000/cinemas/${id}/`, {
+            method: 'DELETE',
+        });
+        setTheaters(prev => prev.filter(t => t.id !== id));
+    };
+
+    const renderItem = ({ item }: { item: Theater }) => (
+        <View style={styles.card}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.description}>{item.address}</Text>
+            <Text style={styles.description}>Funcionamento: {item.openingHours}</Text>
+            <Text style={styles.description}>{item.contactNumber}</Text>
+            <div style={styles.button}>
+                <TouchableOpacity
+                    style={styles.deleteButton}
+                    onPress={() => handleDelete(item.id)}
+                >
+                    <Ionicons name="trash" size={24} color="#fff" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.editButton}
+                    onPress={() => navigation.navigate('EditTheater', { theater: item })}
+                >
+                    <Ionicons name="pencil" size={24} color="#fff" />
+                </TouchableOpacity>
+            </div>
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Cinemas</Text>
+            {loading ? (
+                <ActivityIndicator size="large" color="#FCA311" />
+            ) : (
+                <FlatList
+                    data={theaters}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={renderItem}
+                    contentContainerStyle={{ paddingBottom: 20 }}
+                />
+            )}
+            <TouchableOpacity
+                style={styles.fab}
+                onPress={() => navigation.navigate('CreateTheater')}
+            >
+                <Ionicons name="add" size={28} color="#fff" />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 16,
+        paddingTop: 16,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginBottom: 12,
+        color: '#14213D',
+        alignSelf: 'center',
+    },
+    card: {
+        backgroundColor: '#E5E5E5',
+        padding: 16,
+        borderRadius: 10,
+        marginBottom: 12,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 4,
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#14213D',
+    },
+    description: {
+        fontSize: 14,
+        color: '#666',
+        marginTop: 4,
+    },
+    editButton: {
+        backgroundColor: '#FCA311',
+        padding: 8,
+        borderRadius: 6,
+        marginRight: 8,
+    },
+    editText: {
+        color: '#fff',
+        fontWeight: '500'
+    },
+    fab: {
+        position: 'absolute',
+        right: 20,
+        bottom: 20,
+        backgroundColor: '#14213D',
+        borderRadius: 28,
+        padding: 14,
+        elevation: 4,
+    },
+    deleteButton: {
+        backgroundColor: '#a00f0f',
+        padding: 8,
+        borderRadius: 6,
+        marginRight: 8,
+    },
+    row: {
+        flexDirection: 'row',
+        marginTop: 8,
+        alignSelf: 'flex-end'
+    },
+    button: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 8,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        right: 20,
+        bottom: 10,
+    }
+});
+
+export default TheaterScreen;
